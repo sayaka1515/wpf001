a5ba20a58682bc1a05706f5a645c921fd3473c52
sayaka1515 <5b1g0009@stust.edu.tw>
2024-10-09 10:58:49 +0800

    更新 MainWindow 標題、尺寸及新增訂購功能
    更新了 MainWindow.xaml 的標題，從 "MainWindow" 改為 "飲料點餐系統ver2"。調整了窗口尺寸，從高度 450 和寬度 800 改為高度 540 和寬度 715。新增了多個 StackPanel，每個 StackPanel 包含飲料名稱標籤、價格標籤、滑動條（Slider）和顯示滑動條值的標籤。新增了一個名為 OrderButton 的按鈕，內容為 "確定訂購"。新增了一個名為 ResultTextBlock 的 TextBlock，用於顯示結果。在 MainWindow.xaml.cs 中新增了一個 Dictionary<string, int>，用於存儲飲料名稱及其價格。新增了一個 Slider_ValueChanged 方法，當滑動條值改變時，顯示相應飲料的數量和總價。移除了 MainWindow.xaml.cs 中的 TextBox_TextChanged 方法的註釋代碼。

--- wpf001/MainWindow.xaml.cs
+++ wpf001/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,48 @@ namespace wpf001
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<string, int> drinks = new Dictionary<string, int>
+        {
+            { "紅茶大杯", 60 },
+            { "紅茶小杯", 40 },
+            { "綠茶大杯", 50 },
+            { "綠茶小杯", 30 },
+            { "可樂大杯", 50 },
+            { "可樂小杯", 30 },
+        };
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var targetSlider = sender as Slider;
+            int amount = (int)targetSlider.Value;
+            var targetStackPanel = targetSlider.Parent as StackPanel;
+            var targetLabel = targetStackPanel.Children[0] as Label;
+            var drinkName = targetLabel.Content.ToString();
+            MessageBox.Show(drinkName + " " + amount + "杯，共" + drinks[drinkName] * amount + "元");
+        }
+
+        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    var targetTextBox = sender as TextBox;
+
+        //    //MessageBox.Show(targetTextBox.Text);
+        //    int amount;
+        //    bool success = int.TryParse(targetTextBox.Text, out amount);
+
+        //    if (!success) MessageBox.Show("請輸入數字", "輸入錯誤");
+        //    else if (amount <= 0) MessageBox.Show("請輸入正整數", "輸入錯誤");
+        //    else
+        //    {
+        //        var targetStackPanel = targetTextBox.Parent as StackPanel;
+        //        var targetLabel = targetStackPanel.Children[0] as Label;
+        //        var drinkName = targetLabel.Content.ToString();
+
+        //        MessageBox.Show(drinkName + " " + amount + "杯，共" + drinks[drinkName] * amount + "元");
+        //    }
+        //}
     }
 }
